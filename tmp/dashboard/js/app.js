e830ef3eb998d0e7398c0617dfb503a2eed01dea
jvaz11 <juanvme123@gmail.com>
2015-12-14 22:58:07 -0800

    Updated SASS

--- tmp/dashboard/js/app.js
+++ tmp/dashboard/js/app.js
@@ -38352,107 +38352,6 @@ angular.module('simple-sign').controller('SignsController',
         }]);
     });
 
-angular.module('simple-sign').directive('focusOn', function() {
-   return function(scope, elem, attr) {
-      scope.$on('focusOn', function(e, name) {
-        if(name === attr.focusOn) {
-          elem[0].focus();
-        }
-      });
-   };
-});
-
-angular.module('simple-sign').factory('focus', function ($rootScope, $timeout) {
-  return function(name) {
-    $timeout(function (){
-      $rootScope.$broadcast('focusOn', name);
-    });
-  }
-});
-
-
-
-angular.module('simple-sign').factory('WebPageService', function ($log, $enplugAccount, $q) {
-    'use strict';
-
-    var assetName = 'WebURL';
-
-    return {
-
-        newPage: function () {
-            return {
-                Value: {
-                    ShowContent: 'url',
-                    Url: null,
-                    OverrideUserAgent: false,
-                    ShowMobileSite: null,
-                    ShowDelay: null,
-                    RefreshInterval: null,
-                    RequireRefresh: false,
-                    AllowJavascript: true,
-                    Username: null,
-                    Password: null,
-                    Token: null,
-                    JavascriptOnload: null,
-                    Scale: 100,
-                    _friendlyName: null
-                }
-            };
-        },
-
-        getAccount: function (id) {
-            return $enplugAccount.getAccount().then(function (account) {
-                // FIXME: this should actually redirect to "Not found" page using the SDK
-                return account || $q.reject();
-            });
-        },
-        loadWebPage: function (id) {
-            return $enplugAccount.getAssets().then(function (pages) {
-                var page = pages.filter(function (_page) { return _page.Id === id; })[0];
-
-                // FIXME: this should actually redirect to "Not found" page using the SDK
-                return page || $q.reject();
-            });
-        },
-
-        loadWebPages: function () {
-            return $enplugAccount.getAssets().then(function (pages) {
-                pages.forEach(function (page) {
-                    if (page.Value.Url === null) {
-                        console.log('skipping null asset');
-                    }
-                    else {
-                    page._created = moment(page.Created).format('MMM DD, YYYY');
-                    page.Value.Name = page.Value.Name || page.Value.Url.substring(0, 30);
-                }
-                });
-                return pages;
-            });
-        },
-
-        createWebPage: function (page) {
-            return $enplugAccount.createAsset(assetName, page.Value).then(function (response) {
-            }, function () {
-                return $q.reject('Error saving website "' + page.Value.Name + '"');
-            });
-        },
-
-        updateWebPage: function (page) {
-            return $enplugAccount.updateAsset(page.Id, page.Value).then(function () {
-            }, function () {
-                return $q.reject('Error saving website "' + page.Value.Name + '"');
-            });
-        },
-
-        deleteWebPage: function (page) {
-            return $enplugAccount.removeAsset(page.Id).then(function () {
-            }, function () {
-                return $q.reject('Error deleting website "' + page.Value.Name + '"');
-            });
-        }
-    }
-});
-
 angular.module('simple-sign').factory('GradientService', function() {
     var gradients = [{
         "name": "Intuitive Purple",
@@ -38860,15 +38759,171 @@ angular.module('simple-sign').factory('GradientService', function() {
         getRandomGradient: getRandomGradient
     }
 });
+angular.module('simple-sign').directive('focusOn', function() {
+   return function(scope, elem, attr) {
+      scope.$on('focusOn', function(e, name) {
+        if(name === attr.focusOn) {
+          elem[0].focus();
+        }
+      });
+   };
+});
+
+angular.module('simple-sign').factory('focus', function ($rootScope, $timeout) {
+  return function(name) {
+    $timeout(function (){
+      $rootScope.$broadcast('focusOn', name);
+    });
+  }
+});
+
+
+
+angular.module('simple-sign').factory('WebPageService', function ($log, $enplugAccount, $q) {
+    'use strict';
+
+    var assetName = 'WebURL';
+
+    return {
+
+        newPage: function () {
+            return {
+                Value: {
+                    ShowContent: 'url',
+                    Url: null,
+                    OverrideUserAgent: false,
+                    ShowMobileSite: null,
+                    ShowDelay: null,
+                    RefreshInterval: null,
+                    RequireRefresh: false,
+                    AllowJavascript: true,
+                    Username: null,
+                    Password: null,
+                    Token: null,
+                    JavascriptOnload: null,
+                    Scale: 100,
+                    _friendlyName: null
+                }
+            };
+        },
+
+        getAccount: function (id) {
+            return $enplugAccount.getAccount().then(function (account) {
+                // FIXME: this should actually redirect to "Not found" page using the SDK
+                return account || $q.reject();
+            });
+        },
+        loadWebPage: function (id) {
+            return $enplugAccount.getAssets().then(function (pages) {
+                var page = pages.filter(function (_page) { return _page.Id === id; })[0];
+
+                // FIXME: this should actually redirect to "Not found" page using the SDK
+                return page || $q.reject();
+            });
+        },
+
+        loadWebPages: function () {
+            return $enplugAccount.getAssets().then(function (pages) {
+                pages.forEach(function (page) {
+                    if (page.Value.Url === null) {
+                        console.log('skipping null asset');
+                    }
+                    else {
+                    page._created = moment(page.Created).format('MMM DD, YYYY');
+                    page.Value.Name = page.Value.Name || page.Value.Url.substring(0, 30);
+                }
+                });
+                return pages;
+            });
+        },
+
+        createWebPage: function (page) {
+            return $enplugAccount.createAsset(assetName, page.Value).then(function (response) {
+            }, function () {
+                return $q.reject('Error saving website "' + page.Value.Name + '"');
+            });
+        },
+
+        updateWebPage: function (page) {
+            return $enplugAccount.updateAsset(page.Id, page.Value).then(function () {
+            }, function () {
+                return $q.reject('Error saving website "' + page.Value.Name + '"');
+            });
+        },
+
+        deleteWebPage: function (page) {
+            return $enplugAccount.removeAsset(page.Id).then(function () {
+            }, function () {
+                return $q.reject('Error deleting website "' + page.Value.Name + '"');
+            });
+        }
+    }
+});
+
 angular.module('dashboard-web-page-app-templates', []).run(['$templateCache', function($templateCache) {
-  $templateCache.put("dashboard/templates/searchbox.tpl",
-    "<div class=\"row\">\n" +
-    "    <div class=\"col-md-12\">\n" +
-    "        <form class=\"form-material\">\n" +
-    "            <input type=\"text\" placeholder=\"Enter a city or zip code.\" class=\"form-control\" id=\"searchBox\">\n" +
-    "        </form>\n" +
+  $templateCache.put("dashboard/templates/sign-creator.tpl",
+    "<form class=\"form-material sign-preview\">\n" +
+    "    <div class=\"row\">\n" +
+    "        <div class=\"col-sm-9\">\n" +
+    "            <div class=\"card sign-creator\">\n" +
+    "                <div class=\"card-content\">\n" +
+    "                    <h2 class=\"text-center\">New Sign</h2>\n" +
+    "                    <div class=\"row\">\n" +
+    "                        <div class=\"col-md-4\">\n" +
+    "                            <div class=\"form-group\">\n" +
+    "                                <!-- <label for=\"comment\">Comment:</label> -->\n" +
+    "                                <textarea ng-model=\"sign.headline\" field=\"headerAsset.Value.Title\" class=\"form-control\" rows=\"15\" id=\"comment\" maxlength=180 focus-on=\"mainTextArea\" placeholder=\"Say anything you\\'d like to say!\"></textarea>\n" +
+    "                            </div>\n" +
+    "                        </div>\n" +
+    "                        <!-- Preview container -->\n" +
+    "                        <div class=\"col-md-8\">\n" +
+    "                            <div class=\"preview-container\">\n" +
+    "                                <div class=\"preview\">\n" +
+    "                                    <div class=\"preview-wrapper\" ng-class=\"landscapePreview ? 'portrait' : 'landscape'\">\n" +
+    "                                        <!-- landscape preview -->\n" +
+    "                                        <div class=\"landscape-preview\" ng-if=\"landscapePreview\" ng-style=\"previewGradient\">\n" +
+    "                                            <span class=\"title\" style=\"white-space: pre-wrap;\">{{ sign.headline  | limitTo: 180 }}</span>\n" +
+    "                                        </div>\n" +
+    "                                        <!-- portrait preview -->\n" +
+    "                                        <div class=\"portrait-preview\" ng-if=\"!landscapePreview\" ng-style=\"previewGradient\">\n" +
+    "                                            <span class=\"title\">{{ sign.headline  | limitTo: 180 }}</span>\n" +
+    "                                        </div>\n" +
+    "                                    </div>\n" +
+    "                                </div>\n" +
+    "                            </div>\n" +
+    "                        </div>\n" +
+    "                    </div>\n" +
+    "                </div>\n" +
+    "            </div>\n" +
+    "        </div>\n" +
+    "        <div class=\"col-sm-3\">\n" +
+    "            <div class=\"card card-aside\">\n" +
+    "                <h4>Preview Orientation</h4>\n" +
+    "                <!-- This allows them to choose whether it's landscape or portrait. Should we get their screen orientation automatically instead? -->\n" +
+    "                <div class=\"card-content pv bb\" style=\"display: flex; flex-flow: center;\">\n" +
+    "                    <div class=\"text-center style=\" margin: auto;padding-top: 10px;padding-bottom: 10px; \"\" style=\"margin: auto;\">\n" +
+    "                        <a ng-click=\"landscapePreview = true\">Landscape</a> | <a ng-click=\"landscapePreview = false\">Portrait</a></div>\n" +
+    "                    <br>\n" +
+    "                </div>\n" +
+    "            </div>\n" +
+    "            <div class=\"card card-aside\">\n" +
+    "                <h4>Get New Color</h4>\n" +
+    "                <div class=\"card-content pv bb\" style=\"display: flex; flex-flow: center;\">\n" +
+    "                    <div class=\"text-center\" style=\"margin: auto;\">\n" +
+    "                        <button type=\"button\" ng-click=\"getNewRandomGradient()\" class=\"btn btn-primary btn-circle btn-lg ion-ios-color-wand-outline\"></button>\n" +
+    "                    </div>\n" +
+    "                </div>\n" +
+    "            </div>\n" +
+    "            <div class=\"card-content pv bb\" style=\"display: flex; flex-flow: center;\">\n" +
+    "                <div class=\"text-center\" style=\"margin: auto;\">\n" +
+    "                    <button class=\"btn btn-lg btn-primary\" ng-click=\"saveSign(sign)\" >Save</button>\n" +
+    "                </div>\n" +
+    "            </div>\n" +
+    "        </div>\n" +
     "    </div>\n" +
-    "</div>\n" +
+    "    </div>\n" +
+    "    </div>\n" +
+    "</form>\n" +
     "");
   $templateCache.put("dashboard/templates/signs.tpl",
     "<div class=\"container\">\n" +
@@ -38922,68 +38977,5 @@ angular.module('dashboard-web-page-app-templates', []).run(['$templateCache', fu
     "        </div>\n" +
     "    </div>\n" +
     "</div>\n" +
-    "");
-  $templateCache.put("dashboard/templates/weather.tpl",
-    "<form class=\"form-material sign-preview\">\n" +
-    "    <div class=\"row\">\n" +
-    "        <div class=\"col-md-3 section-summary\">\n" +
-    "            <h3>Write your Sign</h3>\n" +
-    "            <p>Customize your new sign by typing in whatever text you'd like to appear on the screen.</p>\n" +
-    "            <p><a href=\"http://support.enplug.com/hc/en-us/articles/201437329-About-the-Weather-App\" target=\"_blank\">Learn more</a></p>\n" +
-    "        </div>\n" +
-    "        <div class=\"col-md-9\">\n" +
-    "            <div class=\"card\">\n" +
-    "                <div class=\"card-content\">\n" +
-    "                    <div class=\"row\">\n" +
-    "                        <div ng-class=\"landscapePreview ? 'col-sm-5' : 'col-md-6'\">\n" +
-    "                            \n" +
-    "                            <div id=\"searchBoxParent\" class=\"mt-lg\"></div>\n" +
-    "                            <!-- from Directory app header title entry -->\n" +
-    "                            <input ng-model=\"signtext.headline\"\n" +
-    "                                            type=\"text\"\n" +
-    "                                            class=\"mt-sm pb\"\n" +
-    "                                            field=\"headerAsset.Value.Title\"\n" +
-    "                                            label=\"Enter title text\">\n" +
-    "                            </input>\n" +
-    "                            <label >\n" +
-    "                            </label>\n" +
-    "\n" +
-    "                            <p class=\"text-md text-g\">Text must be less than 80 characters.</p>\n" +
-    "                        </div>\n" +
-    "\n" +
-    "                        <!-- Preview container -->\n" +
-    "                        <div class=\"col-md-6\">\n" +
-    "                            <!-- need to replace this with preview for sign -->\n" +
-    "                            <!-- copied the code for landscape vs portrait preview from directory -->\n" +
-    "                            <div class=\"preview\" ng-class=\"landscapePreview ? 'col-sm-7' : 'col-sm-6'\">\n" +
-    "                                <div class=\"preview-wrapper\" ng-class=\"landscapePreview ? 'landscape' : 'portrait'\">\n" +
-    "                                    \n" +
-    "                                    <!-- landscape preview -->\n" +
-    "                                    <!-- removed table etc - need to remove other styling code that pulls in from the entered assets (altho we might use that later if they want to set the color) -->\n" +
-    "                                    <!-- this just needs to point to the titletext -->\n" +
-    "                                    <!-- Styling set in app.scss -->\n" +
-    "                                    <div class=\"landscape-preview\" ng-if=\"landscapePreview\" ng-style=\"{ 'background-color': 'aqua' }\">\n" +
-    "                                        <div class=\"title\"><span>{{ signtext.headline | limitTo: 80 }}</span></div>\n" +
-    "                                    </div>\n" +
-    "\n" +
-    "                                    <!-- portrait preview -->\n" +
-    "                                    <!-- removed table etc - need to remove other styling code that pulls in from the entered assets (altho we might use that later if they want to set the color) -->\n" +
-    "                                    <!-- this just needs to point to the titletext -->\n" +
-    "                                    <!-- Styling set in app.scss -->\n" +
-    "                                    <div class=\"portrait-preview\" ng-if=\"!landscapePreview\" ng-style=\"{ 'background-color': 'black'}\">\n" +
-    "                                        <div class=\"title\"><span>{{ signtext.headline | limitTo: 80 }}</span></div>\n" +
-    "                                    </div>\n" +
-    "\n" +
-    "                                    <!-- This allows them to choose whether it's landscape or portrait. Should we get their screen orientation automatically instead? -->\n" +
-    "                                    <p class=\"tc mt-lg\">Preview: <a ng-click=\"landscapePreview = true\">Landscape</a> or <a ng-click=\"landscapePreview = false\">Portrait</a></p>\n" +
-    "                                </div>\n" +
-    "                            </div>\n" +
-    "                        </div>\n" +
-    "                    </div>\n" +
-    "                </div>\n" +
-    "            </div>\n" +
-    "        </div>\n" +
-    "    </div>\n" +
-    "</form>\n" +
     "");
 }]);
--- tmp/dashboard/js/templates.js
+++ tmp/dashboard/js/templates.js
@@ -1,12 +1,67 @@
 angular.module('dashboard-web-page-app-templates', []).run(['$templateCache', function($templateCache) {
-  $templateCache.put("dashboard/templates/searchbox.tpl",
-    "<div class=\"row\">\n" +
-    "    <div class=\"col-md-12\">\n" +
-    "        <form class=\"form-material\">\n" +
-    "            <input type=\"text\" placeholder=\"Enter a city or zip code.\" class=\"form-control\" id=\"searchBox\">\n" +
-    "        </form>\n" +
+  $templateCache.put("dashboard/templates/sign-creator.tpl",
+    "<form class=\"form-material sign-preview\">\n" +
+    "    <div class=\"row\">\n" +
+    "        <div class=\"col-sm-9\">\n" +
+    "            <div class=\"card sign-creator\">\n" +
+    "                <div class=\"card-content\">\n" +
+    "                    <h2 class=\"text-center\">New Sign</h2>\n" +
+    "                    <div class=\"row\">\n" +
+    "                        <div class=\"col-md-4\">\n" +
+    "                            <div class=\"form-group\">\n" +
+    "                                <!-- <label for=\"comment\">Comment:</label> -->\n" +
+    "                                <textarea ng-model=\"sign.headline\" field=\"headerAsset.Value.Title\" class=\"form-control\" rows=\"15\" id=\"comment\" maxlength=180 focus-on=\"mainTextArea\" placeholder=\"Say anything you\\'d like to say!\"></textarea>\n" +
+    "                            </div>\n" +
+    "                        </div>\n" +
+    "                        <!-- Preview container -->\n" +
+    "                        <div class=\"col-md-8\">\n" +
+    "                            <div class=\"preview-container\">\n" +
+    "                                <div class=\"preview\">\n" +
+    "                                    <div class=\"preview-wrapper\" ng-class=\"landscapePreview ? 'portrait' : 'landscape'\">\n" +
+    "                                        <!-- landscape preview -->\n" +
+    "                                        <div class=\"landscape-preview\" ng-if=\"landscapePreview\" ng-style=\"previewGradient\">\n" +
+    "                                            <span class=\"title\" style=\"white-space: pre-wrap;\">{{ sign.headline  | limitTo: 180 }}</span>\n" +
+    "                                        </div>\n" +
+    "                                        <!-- portrait preview -->\n" +
+    "                                        <div class=\"portrait-preview\" ng-if=\"!landscapePreview\" ng-style=\"previewGradient\">\n" +
+    "                                            <span class=\"title\">{{ sign.headline  | limitTo: 180 }}</span>\n" +
+    "                                        </div>\n" +
+    "                                    </div>\n" +
+    "                                </div>\n" +
+    "                            </div>\n" +
+    "                        </div>\n" +
+    "                    </div>\n" +
+    "                </div>\n" +
+    "            </div>\n" +
+    "        </div>\n" +
+    "        <div class=\"col-sm-3\">\n" +
+    "            <div class=\"card card-aside\">\n" +
+    "                <h4>Preview Orientation</h4>\n" +
+    "                <!-- This allows them to choose whether it's landscape or portrait. Should we get their screen orientation automatically instead? -->\n" +
+    "                <div class=\"card-content pv bb\" style=\"display: flex; flex-flow: center;\">\n" +
+    "                    <div class=\"text-center style=\" margin: auto;padding-top: 10px;padding-bottom: 10px; \"\" style=\"margin: auto;\">\n" +
+    "                        <a ng-click=\"landscapePreview = true\">Landscape</a> | <a ng-click=\"landscapePreview = false\">Portrait</a></div>\n" +
+    "                    <br>\n" +
+    "                </div>\n" +
+    "            </div>\n" +
+    "            <div class=\"card card-aside\">\n" +
+    "                <h4>Get New Color</h4>\n" +
+    "                <div class=\"card-content pv bb\" style=\"display: flex; flex-flow: center;\">\n" +
+    "                    <div class=\"text-center\" style=\"margin: auto;\">\n" +
+    "                        <button type=\"button\" ng-click=\"getNewRandomGradient()\" class=\"btn btn-primary btn-circle btn-lg ion-ios-color-wand-outline\"></button>\n" +
+    "                    </div>\n" +
+    "                </div>\n" +
+    "            </div>\n" +
+    "            <div class=\"card-content pv bb\" style=\"display: flex; flex-flow: center;\">\n" +
+    "                <div class=\"text-center\" style=\"margin: auto;\">\n" +
+    "                    <button class=\"btn btn-lg btn-primary\" ng-click=\"saveSign(sign)\" >Save</button>\n" +
+    "                </div>\n" +
+    "            </div>\n" +
+    "        </div>\n" +
     "    </div>\n" +
-    "</div>\n" +
+    "    </div>\n" +
+    "    </div>\n" +
+    "</form>\n" +
     "");
   $templateCache.put("dashboard/templates/signs.tpl",
     "<div class=\"container\">\n" +
@@ -60,68 +115,5 @@ angular.module('dashboard-web-page-app-templates', []).run(['$templateCache', fu
     "        </div>\n" +
     "    </div>\n" +
     "</div>\n" +
-    "");
-  $templateCache.put("dashboard/templates/weather.tpl",
-    "<form class=\"form-material sign-preview\">\n" +
-    "    <div class=\"row\">\n" +
-    "        <div class=\"col-md-3 section-summary\">\n" +
-    "            <h3>Write your Sign</h3>\n" +
-    "            <p>Customize your new sign by typing in whatever text you'd like to appear on the screen.</p>\n" +
-    "            <p><a href=\"http://support.enplug.com/hc/en-us/articles/201437329-About-the-Weather-App\" target=\"_blank\">Learn more</a></p>\n" +
-    "        </div>\n" +
-    "        <div class=\"col-md-9\">\n" +
-    "            <div class=\"card\">\n" +
-    "                <div class=\"card-content\">\n" +
-    "                    <div class=\"row\">\n" +
-    "                        <div ng-class=\"landscapePreview ? 'col-sm-5' : 'col-md-6'\">\n" +
-    "                            \n" +
-    "                            <div id=\"searchBoxParent\" class=\"mt-lg\"></div>\n" +
-    "                            <!-- from Directory app header title entry -->\n" +
-    "                            <input ng-model=\"signtext.headline\"\n" +
-    "                                            type=\"text\"\n" +
-    "                                            class=\"mt-sm pb\"\n" +
-    "                                            field=\"headerAsset.Value.Title\"\n" +
-    "                                            label=\"Enter title text\">\n" +
-    "                            </input>\n" +
-    "                            <label >\n" +
-    "                            </label>\n" +
-    "\n" +
-    "                            <p class=\"text-md text-g\">Text must be less than 80 characters.</p>\n" +
-    "                        </div>\n" +
-    "\n" +
-    "                        <!-- Preview container -->\n" +
-    "                        <div class=\"col-md-6\">\n" +
-    "                            <!-- need to replace this with preview for sign -->\n" +
-    "                            <!-- copied the code for landscape vs portrait preview from directory -->\n" +
-    "                            <div class=\"preview\" ng-class=\"landscapePreview ? 'col-sm-7' : 'col-sm-6'\">\n" +
-    "                                <div class=\"preview-wrapper\" ng-class=\"landscapePreview ? 'landscape' : 'portrait'\">\n" +
-    "                                    \n" +
-    "                                    <!-- landscape preview -->\n" +
-    "                                    <!-- removed table etc - need to remove other styling code that pulls in from the entered assets (altho we might use that later if they want to set the color) -->\n" +
-    "                                    <!-- this just needs to point to the titletext -->\n" +
-    "                                    <!-- Styling set in app.scss -->\n" +
-    "                                    <div class=\"landscape-preview\" ng-if=\"landscapePreview\" ng-style=\"{ 'background-color': 'aqua' }\">\n" +
-    "                                        <div class=\"title\"><span>{{ signtext.headline | limitTo: 80 }}</span></div>\n" +
-    "                                    </div>\n" +
-    "\n" +
-    "                                    <!-- portrait preview -->\n" +
-    "                                    <!-- removed table etc - need to remove other styling code that pulls in from the entered assets (altho we might use that later if they want to set the color) -->\n" +
-    "                                    <!-- this just needs to point to the titletext -->\n" +
-    "                                    <!-- Styling set in app.scss -->\n" +
-    "                                    <div class=\"portrait-preview\" ng-if=\"!landscapePreview\" ng-style=\"{ 'background-color': 'black'}\">\n" +
-    "                                        <div class=\"title\"><span>{{ signtext.headline | limitTo: 80 }}</span></div>\n" +
-    "                                    </div>\n" +
-    "\n" +
-    "                                    <!-- This allows them to choose whether it's landscape or portrait. Should we get their screen orientation automatically instead? -->\n" +
-    "                                    <p class=\"tc mt-lg\">Preview: <a ng-click=\"landscapePreview = true\">Landscape</a> or <a ng-click=\"landscapePreview = false\">Portrait</a></p>\n" +
-    "                                </div>\n" +
-    "                            </div>\n" +
-    "                        </div>\n" +
-    "                    </div>\n" +
-    "                </div>\n" +
-    "            </div>\n" +
-    "        </div>\n" +
-    "    </div>\n" +
-    "</form>\n" +
     "");
 }]);
