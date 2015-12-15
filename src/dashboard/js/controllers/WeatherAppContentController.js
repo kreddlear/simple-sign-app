acca7ab72646d08a4077f630b9bd7238c6c9138d
jvaz11 <juanvme123@gmail.com>
2015-12-14 19:53:59 -0800

    Refactored and cleaned up signs controller
    - changed header button logic
    - added logic to check for assets and pages

--- src/dashboard/js/controllers/WeatherAppContentController.js
+++ src/dashboard/js/controllers/WeatherAppContentController.js
@@ -1,49 +1,66 @@
-angular.module('weather').controller('WeatherAppContentController',
-    function ($scope, $enplugDashboard, WeatherService, $location, DetectChanges, $firebaseArray) {
-        'use strict';
+angular.module('simple-sign').controller('SignsController',
+    function($scope, WebPageService, pages, accountId, $log, $enplugDashboard, $location, $firebaseArray) {
+        "use strict";
 
         $enplugDashboard.pageLoading(false);
 
-        // $scope.settings = settings;
+        // If no assets/pages exist, create an asset 
+        if (!pages.length) {
 
-        /*
-        * Todo:
-        * create new firebase obj, save var as firebase array
-        * iterate through it in the partial to list them all in a list
-        * add a thumbnail switcher
-        */
+            // Initialize page object. This object will eventually be the asset
+            var page = {
+                Value: {
+                    ShowContent: 'url',
+                    Url: null,
+                    _friendlyName: null
+                }
+            };
 
-        var url = 'https://simplesign.firebaseio.com/accounts/katie/slides';
+            var promise;
 
-        var signsRef = new Firebase(url);
+            // URL composed of base URL + the accountId
+            page.Value.Url = "https://simplesign.firebaseapp.com/#/display/" + accountId;
 
-        $scope.signs = $firebaseArray(signsRef);
-
-        $scope.sign = {
-            color: '',
-            headline: '',
-            id: '',
-            src: ''
-        };
-
-        function save() {
-            $enplugDashboard.loadingIndicator('Updating settings');
-            return WeatherService.saveSettings($scope.settings).then(function (settings) {
-                $enplugDashboard.successIndicator('Updated settings').then(function () {
-                    $scope.displaySearch = false;
-                    watchChanges();
+            // Create the asset with the display URL (this will only happen the first time the app is initialized)
+            promise = WebPageService.createWebPage(page);
+            promise.then(function() {
+                $enplugDashboard.successIndicator('Created new Simple Sign collection.').then(function() {
+                    
+                    // Routes to signs view
+                    $location.path('/');
                 });
             }, $enplugDashboard.errorIndicator);
         }
 
-        $scope.routeToSettings = function () {
-            $location.path('/');
-        };
-
-        // From News App - declares and calls
-        $enplugDashboard.setHeaderButtons([
-            { text: 'Create a Sign', action: $scope.routeToSettings, class: 'btn-primary'}
-        ]);
+        // Give the Scope the accountId to create the URL with. See signs template for implementation
+        $scope.accountId = accountId;
         
-    }
-);
+        // Firebase
+        var signsRef = new Firebase("https://simplesign.firebaseio.com/accounts/" + accountId + "/slides");
+
+        $scope.signs = $firebaseArray(signsRef);
+
+
+        // Header buttons
+
+        // Route to signs in response to click of My Signs header button
+        function viewSigns() {
+            $location.path('/');
+        }
+
+        // Route to create in response to click of Create header button
+        function goToCreateSign() {
+            $location.path('/create');
+        }
+
+        $enplugDashboard.setHeaderButtons([{
+            text: 'My Signs',
+            action: viewSigns,
+            class: 'btn-default ion-android-list',
+            disabled: true
+        }, {
+            text: 'Create',
+            action: goToCreateSign,
+            class: 'btn-default ion-android-color-palette'
+        }]);
+    });
